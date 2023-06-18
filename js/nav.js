d4cb1ff5aabd914d31d0c1d7a0012cde5643fcde
Facundo Cuadrado <hola9811@hotmail.com>
2023-06-18 14:17:18 -0300

    Mejor estructura. Search bar. Nav icons

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,17 @@
 const boton = document.querySelector('#boton_expandir');
+const cerrar = document.querySelector('#boton_cerrar');
 const menu = document.querySelector('#menu_navegacion');
 
 boton.addEventListener('click', ()=> {
     menu.classList.toggle("menu_expandido");
     menu.classList.toggle("menu_colapsado");
-    console.log("dea");
+
+    document.querySelector('body').classList.toggle("body_expandido");
+});
+
+cerrar.addEventListener('click', ()=> {
+    menu.classList.toggle("menu_expandido");
+    menu.classList.toggle("menu_colapsado");
 
     document.querySelector('body').classList.toggle("body_expandido");
 });
