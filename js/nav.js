4ee91b39114c34edd868e4777628756ad5332918
Facundo Cuadrado <hola9811@hotmail.com>
2023-06-20 13:41:12 -0300

    Scrolls bars. Button. Dropdown colapsed. Media queries

--- js/nav.js
+++ js/nav.js
@@ -1,29 +1,35 @@
-const boton = document.querySelector('#boton_expandir');
-const menu = document.querySelector('#menu_navegacion');
-const logo = document.querySelector('#lost_world');
+const boton = document.querySelector("#boton_expandir");
+const menu = document.querySelector("#menu_navegacion");
+const logo = document.querySelector("#lost_world");
 
-const dropdown_boton = document.querySelectorAll('.dropdown_open');
-const dropdown = document.querySelectorAll('.dropdown');
+const dropdown_boton = document.querySelectorAll(".dropdown_open");
+const dropdown = document.querySelectorAll(".dropdown");
 
-boton.addEventListener('click', () => {
-  menu.classList.toggle("menu_expandido");
-  menu.classList.toggle("menu_colapsado");
-  document.querySelector('body').classList.toggle("body_expandido");
+boton.addEventListener("click", () => {
+  abrirNav();
 });
 
-lost_world.addEventListener('click', () => {
+lost_world.addEventListener("click", () => {
   menu.classList.toggle("menu_expandido");
   menu.classList.toggle("menu_colapsado");
-  document.querySelector('body').classList.toggle("body_expandido");
+  document.querySelector("body").classList.toggle("body_expandido");
 });
-
 
 for (let i = 0; i < dropdown_boton.length; i++) {
-  dropdown_boton[i].addEventListener('click', () => {
+  dropdown_boton[i].addEventListener("click", () => {
+    if (menu.classList.contains('menu_colapsado')) {
+      abrirNav();
+    }
     var dropdown_boton_actual = dropdown_boton[i];
-    dropdown_boton_actual.classList.toggle('activo_boton');
+    dropdown_boton_actual.classList.toggle("activo_boton");
     var dropdown_actual = dropdown[i];
-    dropdown_actual.classList.toggle('activo');
-    console.log(dropdown_actual);
-  })
+    dropdown_actual.classList.toggle("activo");
+
+  });
+}
+
+function abrirNav () {
+    menu.classList.toggle("menu_expandido");
+    menu.classList.toggle("menu_colapsado");
+    document.querySelector("body").classList.toggle("body_expandido");
 }
